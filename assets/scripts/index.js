77c8a90f6523c6ef9d6fc88193e787c2a48a6c71
escurse <av1967@naver.com>
2024-11-17 19:44:50 +0900

    pofol_H

---
+++ assets/scripts/index.js
@@ -0,0 +1,10 @@
+const $main = document.getElementById('main');
+const $containers = $main.querySelector(':scope > .container > .nav')
+
+const $nav = document.getElementById('nav');
+const $check = $nav.querySelector(':scope > .label > .check');
+
+$check.onclick = () => {
+}
+
+
